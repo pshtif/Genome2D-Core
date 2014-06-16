04a2cc37fc4d2be908aa611deac664ead064b629
Peter "sHTiF" Stefcek <pshtif@gmail.com>
2014-06-16 21:12:09 +0200

    Documentation & Cleanup

--- src/com/genome2d/postprocesses/GFilterPP.hx
+++ src/com/genome2d/postprocesses/GFilterPP.hx
@@ -1,7 +1,8 @@
 /*
- * 	Genome2D - GPU 2D framework utilizing Molehill API
+ * 	Genome2D - 2D GPU Framework
+ * 	http://www.genome2d.com
  *
- *	Copyright 2011 Peter Stefcek. All rights reserved.
+ *	Copyright 2011-2014 Peter Stefcek. All rights reserved.
  *
  *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
  */
--- src/com/genome2d/textures/GTexture.hx
+++ src/com/genome2d/textures/GTexture.hx
@@ -1,10 +1,11 @@
 /*
-* 	Genome2D - GPU 2D framework utilizing Molehill API
-*
-*	Copyright 2011 Peter Stefcek. All rights reserved.
-*
-*	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
-*/
+ * 	Genome2D - 2D GPU Framework
+ * 	http://www.genome2d.com
+ *
+ *	Copyright 2011-2014 Peter Stefcek. All rights reserved.
+ *
+ *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
+ */
 package com.genome2d.textures;
 
 import com.genome2d.context.IContext;
