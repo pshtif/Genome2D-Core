0a34b89835a21801dd8300e908a336a7ab4162e5
Peter "sHTiF" Stefcek <pshtif@gmail.com>
2014-10-08 16:40:51 +0200

    - examples merge

--- src/com/genome2d/Genome2D.hx
+++ src/com/genome2d/Genome2D.hx
@@ -34,7 +34,7 @@ class Genome2D
     /**
         Genome2D Version
     **/
-	inline static public var VERSION:String = "1.0.278";
+	inline static public var VERSION:String = "1.0.279";
 
 	static private var g2d_instance:Genome2D;
 	static private var g2d_instantiable:Bool = false;
