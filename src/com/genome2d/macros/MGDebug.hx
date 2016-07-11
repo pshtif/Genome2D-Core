25e9b38919eaece167dd0d3afef8690f87999c77
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-07-11 17:06:05 +0200

    moved build files added further debug

--- src/com/genome2d/macros/MGDebug.hx
+++ src/com/genome2d/macros/MGDebug.hx
@@ -25,6 +25,10 @@ class MGDebug {
     macro static public function WARNING(?p_arg1, ?p_arg2, ?p_arg3, ?p_arg4, ?p_arg5, ?p_arg6, ?p_arg7, ?p_arg8, ?p_arg9, ?p_arg10, ?p_arg11, ?p_arg12, ?p_arg13, ?p_arg14, ?p_arg15, ?p_arg16, ?p_arg17, ?p_arg18, ?p_arg19, ?p_arg20) {
         return macro @:pos(p_arg1.pos) com.genome2d.debug.GDebug.warning($p_arg1, $p_arg2, $p_arg3, $p_arg4, $p_arg5, $p_arg6, $p_arg7, $p_arg8, $p_arg9, $p_arg10, $p_arg11, $p_arg12, $p_arg13, $p_arg14, $p_arg15, $p_arg16, $p_arg17, $p_arg18, $p_arg19, $p_arg20);
     }
+	
+	macro static public function G2D_WARNING(?p_arg1, ?p_arg2, ?p_arg3, ?p_arg4, ?p_arg5, ?p_arg6, ?p_arg7, ?p_arg8, ?p_arg9, ?p_arg10, ?p_arg11, ?p_arg12, ?p_arg13, ?p_arg14, ?p_arg15, ?p_arg16, ?p_arg17, ?p_arg18, ?p_arg19, ?p_arg20) {
+        return macro @:pos(p_arg1.pos) com.genome2d.debug.GDebug.g2d_warning($p_arg1, $p_arg2, $p_arg3, $p_arg4, $p_arg5, $p_arg6, $p_arg7, $p_arg8, $p_arg9, $p_arg10, $p_arg11, $p_arg12, $p_arg13, $p_arg14, $p_arg15, $p_arg16, $p_arg17, $p_arg18, $p_arg19, $p_arg20);
+    }
 
     macro static public function ERROR(?p_arg1, ?p_arg2, ?p_arg3, ?p_arg4, ?p_arg5, ?p_arg6, ?p_arg7, ?p_arg8, ?p_arg9, ?p_arg10, ?p_arg11, ?p_arg12, ?p_arg13, ?p_arg14, ?p_arg15, ?p_arg16, ?p_arg17, ?p_arg18, ?p_arg19, ?p_arg20) {
         return macro @:pos(p_arg1.pos) com.genome2d.debug.GDebug.error($p_arg1, $p_arg2, $p_arg3, $p_arg4, $p_arg5, $p_arg6, $p_arg7, $p_arg8, $p_arg9, $p_arg10, $p_arg11, $p_arg12, $p_arg13, $p_arg14, $p_arg15, $p_arg16, $p_arg17, $p_arg18, $p_arg19, $p_arg20);
