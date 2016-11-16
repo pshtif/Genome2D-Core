b7bdd7d6e60f0002c6f2ff67fce1eb5c04dca097
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-11-16 16:57:12 +0100

    update script

--- src/com/genome2d/components/GScriptComponent.hx
+++ src/com/genome2d/components/GScriptComponent.hx
@@ -1,5 +1,8 @@
 package com.genome2d.components;
+
+import com.genome2d.context.GCamera;
 import com.genome2d.scripts.GScript;
+
 class GScriptComponent extends GComponent {
     private var g2d_script:GScript = null;
     @prototype("getReference")
@@ -12,17 +15,20 @@ class GScriptComponent extends GComponent {
     inline private function set_script(p_value:GScript):GScript {
         if (g2d_script != null) {
             g2d_script.onInvalidated.remove(invalidate);
+            if (g2d_executeUpdate != null) node.core.onUpdate.remove(update_handler);
             if (g2d_executeDispose != null) g2d_executeDispose();
         }
         g2d_script = p_value;
+        g2d_script.setVariable("node", node);
         g2d_script.onInvalidated.add(invalidate);
         invalidate();
         return g2d_script;
     }
 
-    private var g2d_executeRender:Dynamic;
-    private var g2d_executeInit:Dynamic;
+    private var g2d_executeRender:GCamera->Bool->Void;
+    private var g2d_executeInit:Void->Void;
     private var g2d_executeDispose:Void->Void;
+    private var g2d_executeUpdate:Float->Void;
 
     override public function dispose():Void {
         if (g2d_executeDispose != null) g2d_executeDispose();
@@ -30,11 +36,17 @@ class GScriptComponent extends GComponent {
 
     private function invalidate():Void {
         if (g2d_script != null) {
-            g2d_executeInit = g2d_script.getVariable("spawn");
-            g2d_executeDispose = g2d_script.getVariable("update");
+            g2d_executeInit = g2d_script.getVariable("init");
+            g2d_executeDispose = g2d_script.getVariable("dispose");
             g2d_executeRender = g2d_script.getVariable("render");
+            g2d_executeUpdate = g2d_script.getVariable("update");
         }
 
         if (g2d_executeInit != null) g2d_executeInit();
+        if (g2d_executeUpdate != null) node.core.onUpdate.add(update_handler);
+    }
+
+    private function update_handler(p_deltaTime:Float):Void {
+        if (g2d_executeUpdate != null) g2d_executeUpdate(p_deltaTime);
     }
 }
