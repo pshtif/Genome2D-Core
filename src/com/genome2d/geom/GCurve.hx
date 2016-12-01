c364c306c39a6fe019530ccb1825ddf22021d0a4
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-12-01 09:09:40 +0100

    removed inline on curve to allow overriding

--- src/com/genome2d/geom/GCurve.hx
+++ src/com/genome2d/geom/GCurve.hx
@@ -76,7 +76,7 @@ class GCurve implements IGPrototypable {
         return (g2d_pathLength>0) ? g2d_segments[g2d_pathLength-1].end : Math.NaN;
     }
 
-    inline public function calculate(k:Float):Float {
+    public function calculate(k:Float):Float {
         var r:Float = start;
         if (g2d_pathLength == 1) {
             r = g2d_segments[0].calculate(start, k);
