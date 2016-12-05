a78523974f8ffee9b072fc7e97079c90382d804a
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-12-05 17:20:08 +0100

    added int point

---
+++ src/com/genome2d/geom/GIntPoint.hx
@@ -0,0 +1,14 @@
+package com.genome2d.geom;
+class GIntPoint {
+    public var x:Int = 0;
+    public var y:Int = 0;
+
+    public function new(p_x:Int = 0, p_y:Int = 0) {
+        x = p_x;
+        y = p_y;
+    }
+
+    public function equals(p_point:GIntPoint):Bool {
+        return x == p_point.x && y == p_point.y;
+    }
+}
