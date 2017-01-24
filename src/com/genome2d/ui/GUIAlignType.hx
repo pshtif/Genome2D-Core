0fd086a79b7d128efdc3b1b15815bc990ba5b08e
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-01-24 16:23:21 +0100

    ui align is now Enum instead of Int

---
+++ src/com/genome2d/ui/GUIAlignType.hx
@@ -0,0 +1,13 @@
+package com.genome2d.ui;
+enum GUIAlignType {
+    NONE;
+    TOP_LEFT;
+    TOP_CENTER;
+    TOP_RIGHT;
+    MIDDLE_LEFT;
+    MIDDLE_CENTER;
+    MIDDLE_RIGHT;
+    BOTTOM_LEFT;
+    BOTTOM_CENTER;
+    BOTTOM_RIGHT;
+}
