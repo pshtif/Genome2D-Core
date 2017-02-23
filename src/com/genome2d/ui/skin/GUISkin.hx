207706a45290ed0c2da2cd1ad058347762d72a1b
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-02-23 15:33:10 +0100

    fix null checking

--- src/com/genome2d/ui/skin/GUISkin.hx
+++ src/com/genome2d/ui/skin/GUISkin.hx
@@ -24,7 +24,7 @@ class GUISkin implements IGPrototypable {
     static private function batchRender(p_skin:GUISkin):Bool {
         var batched:Bool = false;
 		if (p_skin.getTexture() == null) {
-		} else if ((p_skin.getTexture().nativeTexture != g2d_currentPriorityTexture.nativeTexture && g2d_currentPriorityTexture != null) ||
+		} else if ((g2d_currentPriorityTexture != null && p_skin.getTexture().nativeTexture != g2d_currentPriorityTexture.nativeTexture) ||
 		           (g2d_currentBatchTexture != null &&
 		  		   !p_skin.getTexture().hasSameGPUTexture(g2d_currentBatchTexture) &&
 					p_skin.getFilter() == g2d_currentBatchFilter)) {
