fd04e05c48451dc69f8354cc3e83f3489f20a98c
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-06-02 17:25:07 +0200

    update head

--- src/com/genome2d/tween/GTween.hx
+++ src/com/genome2d/tween/GTween.hx
@@ -1,3 +1,11 @@
+/*
+ * 	Genome2D - 2D GPU Framework
+ * 	http://www.genome2d.com
+ *
+ *	Copyright 2011-2014 Peter Stefcek. All rights reserved.
+ *
+ *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
+ */
 package com.genome2d.tween;
 import com.genome2d.proto.GPrototype;
 import com.genome2d.proto.GPrototypeFactory;
