fde88a4c8fd71aa9302e50d0a22b3130991a60b6
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-07-03 15:28:25 +0200

    uprate

--- src/com/genome2d/node/GNode.hx
+++ src/com/genome2d/node/GNode.hx
@@ -2,7 +2,7 @@
  * 	Genome2D - 2D GPU Framework
  * 	http://www.genome2d.com
  *
- *	Copyright 2011-2014 Peter Stefcek. All rights reserved.
+ *	Copyright 2011-2017 Peter Stefcek. All rights reserved.
  *
  *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
  */
