7d268f036f0af6d92e885ac20741bdfc508dbea9
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-09-25 15:01:51 +0200

    license updates

--- src/com/genome2d/ui/skin/GUIShapeSkin.hx
+++ src/com/genome2d/ui/skin/GUIShapeSkin.hx
@@ -1,3 +1,11 @@
+/*
+ * 	Genome2D - 2D GPU Framework
+ * 	http://www.genome2d.com
+ *
+ *	Copyright 2011-2015 Peter Stefcek. All rights reserved.
+ *
+ *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
+ */
 package com.genome2d.ui.skin;
 import com.genome2d.context.GBlendMode;
 import com.genome2d.textures.GTexture;
--- src/com/genome2d/ui/skin/GUISkin.hx
+++ src/com/genome2d/ui/skin/GUISkin.hx
@@ -1,3 +1,11 @@
+/*
+ * 	Genome2D - 2D GPU Framework
+ * 	http://www.genome2d.com
+ *
+ *	Copyright 2011-2017 Peter Stefcek. All rights reserved.
+ *
+ *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
+ */
 package com.genome2d.ui.skin;
 import com.genome2d.context.filters.GFilter;
 import com.genome2d.input.GMouseInput;
