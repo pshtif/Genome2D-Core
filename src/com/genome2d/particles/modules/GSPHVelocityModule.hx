17ed3013cd804ad79d50ef23d74958e1888aa5e6
Peter @sHTiF Stefcek <pshtif@gmail.com>
2018-01-23 15:29:16 +0100

    comment out non crucial sph code

--- src/com/genome2d/particles/modules/GSPHVelocityModule.hx
+++ src/com/genome2d/particles/modules/GSPHVelocityModule.hx
@@ -1,5 +1,6 @@
 package com.genome2d.particles.modules;
 
+import com.genome2d.macros.MGDebug;
 import com.genome2d.particles.GParticleEmitter;
 import com.genome2d.particles.GParticle;
 
@@ -21,6 +22,17 @@ class GSPHVelocityModule extends GParticleEmitterModule
 			p_particle.velocityX += p_particle.fluidX / (p_particle.density * 0.9 + 0.1);
 			p_particle.velocityY += p_particle.fluidY / (p_particle.density * 0.9 + 0.1);
 		}
+
+		/*
+		p_particle.velocityY += .2;
+		if (!p_particle.fixed) {
+			p_particle.x += p_particle.velocityX * p_deltaTime/30;
+			p_particle.y += p_particle.velocityY * p_deltaTime / 30;
+		}
+		p_particle.accumulatedTime += p_deltaTime;
+
+		if (p_particle.accumulatedTime > 5000) p_particle.die = true;
+		/**/
 	}
 	
 }
