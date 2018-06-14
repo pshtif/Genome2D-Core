1f8da308d24461bc36fe329ceefb37e7e25f599b
Peter @sHTiF Stefcek <pshtif@gmail.com>
2018-06-14 09:49:49 +0200

    added support for transparent objects sorting

--- src/com/genome2d/g3d/G3DModel.hx
+++ src/com/genome2d/g3d/G3DModel.hx
@@ -2,6 +2,10 @@ package com.genome2d.g3d;
 
 import com.genome2d.context.renderers.G3DRenderer;
 import com.genome2d.geom.GMatrix3D;
+import com.genome2d.geom.GVector3D;
+import com.genome2d.macros.MGDebug;
+import com.genome2d.textures.GTextureManager;
+import com.genome2d.debug.GDebug;
 
 class G3DModel extends G3DNode {
 	public var visible:Bool = true;
@@ -10,10 +14,13 @@ class G3DModel extends G3DNode {
 	
 	public var inheritSceneMatrixMode:G3DMatrixInheritMode = G3DMatrixInheritMode.REPLACE;
 	public var modelMatrix:GMatrix3D;
+	public var transparent:Bool = false;
+	public var center:GVector3D;
 	
 	public function new(p_id:String):Void {
 		super(p_id);
 		
+		center = new GVector3D();
 		modelMatrix = new GMatrix3D();
 	}
 	
@@ -36,5 +43,49 @@ class G3DModel extends G3DNode {
 	public function dispose():Void {
 		if (renderer != null) renderer.dispose();
 		renderer = null;
+	}
+	
+	public function invalidate():Void {
+		// Hack to check for transparency based on name suffix
+		transparent = name.indexOf("_T") == name.length - 2;
+		
+		var geometry:G3DGeometry = getGeometry();
+		if (geometry == null) MGDebug.G2D_ERROR("Model has no geometry.");
+		
+		if (renderer == null) {
+			renderer = new G3DRenderer(geometry.vertices, geometry.uvs, geometry.indices, geometry.normals, false);
+		} else {
+			renderer.invalidateGeometry(geometry.vertices, geometry.uvs, geometry.indices, geometry.normals);
+		}
+		
+		
+		var material:G3DMaterial = getMaterial();
+		if (material == null) MGDebug.G2D_ERROR("Model has no material.");
+		
+		var texture:G3DTexture = getMaterial().getTexture();
+		if (texture == null) {
+			MGDebug.WARNING("Model material has no texture.");
+			renderer.texture = GTextureManager.getTexture("g2d_internal");
+		} else {				
+			var textureId:String = texture.relativePath;
+			renderer.texture = GTextureManager.getTexture(textureId);
+			if (renderer.texture == null) {
+				MGDebug.WARNING("Couldn't find texture", textureId);
+				renderer.texture = GTextureManager.getTexture("g2d_internal");
+			}
+		}
+	}
+	
+	public function calculateCenter():Void {
+		center = new GVector3D();
+		var vertices:Array<Float> = getGeometry().vertices;
+		for (i in 0...Std.int(vertices.length/3)) {
+			center.x += vertices[i * 3];
+			center.y += vertices[i * 3 + 1];
+			center.z += vertices[i * 3 + 2];
+		}
+		center.x /= vertices.length;
+		center.y /= vertices.length;
+		center.z /= vertices.length;
 	}
 }
