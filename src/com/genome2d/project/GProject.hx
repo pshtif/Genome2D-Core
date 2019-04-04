751e98ddabee5c40d830c955a41d22dcbb27bfbe
Peter @sHTiF Stefcek <pshtif@gmail.com>
2019-04-04 16:51:59 +0200

    mouse input added

--- src/com/genome2d/project/GProject.hx
+++ src/com/genome2d/project/GProject.hx
@@ -11,6 +11,7 @@ import com.genome2d.assets.GStaticAssetManager;
 import com.genome2d.geom.GRectangle;
 #if cs
 import unityengine.*;
+import com.genome2d.input.GMouseInputType;
 #end
 
 #if !cs
@@ -21,8 +22,7 @@ class GProject extends MonoBehaviour {
 #end
     #if cs
     public var onFrame(default,null):GCallback0;
-    public var onMouseButton(default,null):GCallback2<Int,Bool>;
-    public var onMouseMove(default,null):GCallback0;
+    public var onMouse(default,null):GCallback2<String, Int>;
     #end
 
     private var g2d_genome:Genome2D;
@@ -49,10 +49,9 @@ class GProject extends MonoBehaviour {
     public function Start() {
         GDebug.info("Starting project.");
         onFrame = new GCallback0();
-        onMouseMove = new GCallback0();
-        onMouseButton = new GCallback2<Int, Bool>();
+        onMouse = new GCallback2<String, Int>();
 
-        var contextConfig:GContextConfig = new GContextConfig(this, new GRectangle(0,0,800,600));
+        var contextConfig:GContextConfig = new GContextConfig(this, new GRectangle(0,0,Screen.width,Screen.height));
         g2d_config = new GProjectConfig(contextConfig);
 
         g2d_genome = Genome2D.getInstance();
@@ -69,18 +68,16 @@ class GProject extends MonoBehaviour {
 
     public function Update() {
         if (Input.GetMouseButtonDown(0)) {
-            onMouseButton.dispatch(0, false);
-        }
-
-        if (Input.GetMouseButtonUp(0)) {
-            onMouseButton.dispatch(0, false);
-        }
-
-        if (!g2d_mouseInitialized) {
-            g2d_mouseInitialized = true;
+            onMouse.dispatch(GMouseInputType.MOUSE_DOWN, 0);
+        } else if (Input.GetMouseButtonUp(0)) {
+            onMouse.dispatch(GMouseInputType.MOUSE_UP, 0);
         } else {
-            if (g2d_lastMouseX != Input.mousePosition.x || g2d_lastMouseY != Input.mousePosition.y) {
-                onMouseMove.dispatch();
+            if (!g2d_mouseInitialized) {
+                g2d_mouseInitialized = true;
+            } else {
+                if (g2d_lastMouseX != Input.mousePosition.x || g2d_lastMouseY != Input.mousePosition.y) {
+                    onMouse.dispatch(GMouseInputType.MOUSE_MOVE, -1);
+                }
             }
         }
         g2d_lastMouseX = Input.mousePosition.x;
