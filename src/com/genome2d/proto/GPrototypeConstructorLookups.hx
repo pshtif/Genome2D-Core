2e429241fc45fcf96f6bdd83f6c9a5c4a0ac152c
sHTiF <pshtif@gmail.com>
2019-04-07 19:41:10 +0200

    added all prototype constructor lookups for non zero constructors

--- src/com/genome2d/proto/GPrototypeConstructorLookups.hx
+++ src/com/genome2d/proto/GPrototypeConstructorLookups.hx
@@ -13,5 +13,12 @@ class GPrototypeConstructorLookups {
         _arguments.set("textureSkin", ["", null, true, null]);
         _arguments.set("fontSkin", ["", null, 1, true, null]);
         _arguments.set("element", [null]);
+        _arguments.set("particle_emitter", [null]);
+        _arguments.set("GCurve", [0.0]);
+        _arguments.set("GIntPoint", [0,0]);
+        _arguments.set("GPostProcess", [1, null]);
+        _arguments.set("transition", ["", 0, null, 0]);
+        _arguments.set("GCurveInterp", [null]);
+        _arguments.set("tweenFloat", [null]);
     }
 }
