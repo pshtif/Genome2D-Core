e5b415c4868fde92f7dd48918eeab233e6489e76
sHTiF <pshtif@gmail.com>
2020-06-30 12:14:54 +0200

    change color to Int due to casting issues withotu generics

--- src/com/genome2d/text/GTextFormat.hx
+++ src/com/genome2d/text/GTextFormat.hx
@@ -6,21 +6,22 @@ package com.genome2d.text;
  */
 class GTextFormat
 {
-	public var g2d_formatMap:Map<Int,UInt>;
+	public var g2d_formatMap:Map<Int,Int>;
 	
 	public function new() {
-		g2d_formatMap = new Map<Int,UInt>();
+		g2d_formatMap = new Map<Int,Int>();
 	}
 	
-	public function setIndexColor(p_index:Int, p_color:UInt):Void {
+	public function setIndexColor(p_index:Int, p_color:Int):Void {
 		g2d_formatMap.set(p_index, p_color);
 	}
 	
-	public function getIndexColor(p_index:Int):UInt {
-		var color:UInt = 0;
+	public function getIndexColor(p_index:Int):Int {
+		var color:Int = 0;
 		if (g2d_formatMap.exists(p_index)) {
 			color = g2d_formatMap.get(p_index);
 		}
+
 		return color;
 	}
 	
